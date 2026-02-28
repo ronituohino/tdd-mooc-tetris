10e7258f252f730165b852ccb91c83b7969c6652
Roni Tuohino <rt@ronituohino.com>
2026-02-28 05:44:02 +0200

    Now back to rotate

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -3,8 +3,8 @@ export class RotatingShape {
   width;
   height;
 
-  constructor(charArr, widht, height) {
-    this.charArr = charArr;
+  constructor(characters, widht, height) {
+    this.characters = characters;
     this.width = widht;
     this.height = height;
   }
@@ -14,15 +14,15 @@ export class RotatingShape {
       return undefined;
     }
 
-    let charArrays = str.split("\n").map((s) => s.trim());
-    return new RotatingShape(charArrays.join(""), charArrays[0].length, charArrays.length);
+    let characterArr = str.split("\n").map((s) => s.trim());
+    return new RotatingShape(characterArr.join(""), characterArr[0].length, characterArr.length);
   }
 
   toString() {
     let str = "";
     for (let y = 0; y < this.height; y++) {
       for (let x = 0; x < this.width; x++) {
-        str += this.charArr[y * this.width + x];
+        str += this.characters[y * this.width + x];
       }
       str += "\n";
     }
