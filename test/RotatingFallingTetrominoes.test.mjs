59596a6e3e8655b5dbaa2d9971e9bc54da54b9d5
Roni Tuohino <rt@ronituohino.com>
2026-03-03 09:02:13 +0200

    Clean up tests

--- test/RotatingFallingTetrominoes.test.mjs
+++ test/RotatingFallingTetrominoes.test.mjs
@@ -1,13 +1,6 @@
 import { beforeEach, describe, test } from "vitest";
 import { expect } from "chai";
 import { Board } from "../src/Board.mjs";
-import { Tetromino } from "../src/Tetromino.mjs";
-
-function fallToBottom(board) {
-  for (let i = 0; i < 10; i++) {
-    board.tick();
-  }
-}
 
 const CUSTOM_T_BLOCK = [
   `.T.
@@ -22,6 +15,29 @@ const CUSTOM_T_BLOCK = [
   `.T.
    TT.
    .T.`,
+];
+
+const CUSTOM_I_BLOCK = [
+  `.....
+   .....
+   IIII.
+   .....
+   .....`,
+  `..I..
+   ..I..
+   ..I..
+   ..I..
+   .....`,
+  `.....
+   .....
+   IIII.
+   .....
+   .....`,
+  `..I..
+   ..I..
+   ..I..
+   ..I..
+   .....`,
 ];
 
 describe("A falling Tetromino can be rotated", () => {
@@ -144,11 +160,18 @@ describe("A falling Tetromino cannot be rotated", () => {
   });
 
   test("left when other blocks are in the way", () => {
-    board.drop(Tetromino.O_SHAPE);
-    fallToBottom(board);
-    board.drop(Tetromino.T_SHAPE);
-    board.tick();
-    board.tick();
+    board.seed(
+      `..........
+       ..........
+       ..........
+       ..........
+       ....OO....
+       ....OO....`,
+      CUSTOM_T_BLOCK,
+      3,
+      2
+    );
+
     board.rotateLeft();
     expect(board.toString()).to.equalShape(
       `..........
@@ -160,12 +183,18 @@ describe("A falling Tetromino cannot be rotated", () => {
     );
   });
   test("right when other blocks are in the way", () => {
-    board.drop(Tetromino.O_SHAPE);
-    fallToBottom(board);
-    board.drop(Tetromino.T_SHAPE);
-    board.moveRight();
-    board.tick();
-    board.tick();
+    board.seed(
+      `..........
+       ..........
+       ..........
+       ..........
+       ....OO....
+       ....OO....`,
+      CUSTOM_T_BLOCK,
+      4,
+      2
+    );
+
     board.rotateRight();
     expect(board.toString()).to.equalShape(
       `..........
@@ -177,20 +206,25 @@ describe("A falling Tetromino cannot be rotated", () => {
     );
   });
   test("when block would reach higher than before (no floor kick)", () => {
-    board.drop(Tetromino.T_SHAPE);
-    board.tick();
-    board.tick();
-    board.tick();
-    board.tick();
-    board.rotateLeft();
+    board.seed(undefined, CUSTOM_T_BLOCK, 1, 4);
+
     board.rotateRight();
     expect(board.toString()).to.equalShape(
       `..........
        ..........
        ..........
        ..........
-       ....T.....
-       ...TTT....`
+       ..T.......
+       .TTT......`
+    );
+    board.rotateLeft();
+    expect(board.toString()).to.equalShape(
+      `..........
+       ..........
+       ..........
+       ..........
+       ..T.......
+       .TTT......`
     );
   });
 });
@@ -202,92 +236,53 @@ describe("Wall kick works", () => {
   });
 
   test("on the left side", () => {
-    board.drop(Tetromino.T_SHAPE);
-    board.rotateRight();
-    board.moveLeft();
-    board.moveLeft();
-    board.moveLeft();
-    board.moveLeft();
-    board.tick();
-    expect(board.toString()).to.equalShape(
-      `........
-       T.......
-       TT......
-       T.......
-       ........
-       ........`
-    );
+    board.seed(undefined, CUSTOM_T_BLOCK, -1, 0, 1);
+
     board.rotateLeft();
     expect(board.toString()).to.equalShape(
-      `........
-       .T......
+      `.T......
        TTT.....
+       ........
        ........
        ........
        ........`
     );
   });
   test("on the right side", () => {
-    board.drop(Tetromino.T_SHAPE);
-    board.rotateLeft();
-    board.moveRight();
-    board.moveRight();
-    board.moveRight();
-    board.moveRight();
-    board.tick();
-    expect(board.toString()).to.equalShape(
-      `........
-       .......T
-       ......TT
-       .......T
-       ........
-       ........`
-    );
+    board.seed(undefined, CUSTOM_T_BLOCK, 6, 0, 3);
+
     board.rotateRight();
     expect(board.toString()).to.equalShape(
-      `........
-       ......T.
+      `......T.
        .....TTT
+       ........
        ........
        ........
        ........`
     );
   });
   test("with an I shape on the left", () => {
-    board.drop(Tetromino.I_SHAPE);
-    board.rotateRight();
-    board.tick();
-    board.tick();
-    board.tick();
-    board.moveLeft();
-    board.moveLeft();
-    board.moveLeft();
+    board.seed(undefined, CUSTOM_I_BLOCK, -2, 0, 1);
+
     board.rotateLeft();
     expect(board.toString()).to.equalShape(
       `........
        ........
-       ........
        IIII....
+       ........
        ........
        ........`
     );
   });
   test("with an I shape on the right", () => {
-    board.drop(Tetromino.I_SHAPE);
-    board.rotateRight();
-    board.tick();
-    board.tick();
-    board.tick();
-    board.moveRight();
-    board.moveRight();
-    board.moveRight();
-    board.moveRight();
+    board.seed(undefined, CUSTOM_I_BLOCK, 5, 0, 1);
+
     board.rotateRight();
     expect(board.toString()).to.equalShape(
       `........
        ........
-       ........
        ....IIII
+       ........
        ........
        ........`
     );
@@ -301,18 +296,21 @@ describe("Wall kick does not work on", () => {
   });
 
   test("the left side if it is blocked", () => {
-    board.drop(Tetromino.I_SHAPE);
-    board.rotateRight();
-    board.moveLeft();
-    fallToBottom(board);
-    board.drop(Tetromino.T_SHAPE);
-    board.rotateRight();
-    board.moveLeft();
-    board.moveLeft();
-    board.moveLeft();
-    board.tick();
-    board.tick();
-    board.tick();
+    board.seed(
+      `........
+       ........
+       ........
+       ........
+       ..I.....
+       ..I.....
+       ..I.....
+       ..I.....`,
+      CUSTOM_T_BLOCK,
+      -1,
+      3,
+      1
+    );
+
     board.rotateLeft();
     expect(board.toString()).to.equalShape(
       `........
@@ -326,27 +324,29 @@ describe("Wall kick does not work on", () => {
     );
   });
   test("the right side if it is blocked", () => {
-    board.drop(Tetromino.I_SHAPE);
-    board.rotateRight();
-    board.moveRight();
-    board.moveRight();
-    fallToBottom(board);
-    board.drop(Tetromino.T_SHAPE);
-    board.rotateRight();
-    board.moveRight();
-    board.moveRight();
-    board.moveRight();
-    board.tick();
-    board.tick();
-    board.tick();
+    board.seed(
+      `........
+       ........
+       ........
+       ........
+       .....I..
+       .....I..
+       .....I..
+       .....I..`,
+      CUSTOM_T_BLOCK,
+      6,
+      3,
+      3
+    );
+
     board.rotateLeft();
     expect(board.toString()).to.equalShape(
       `........
        ........
        ........
-       ......T.
+       .......T
        .....ITT
-       .....IT.
+       .....I.T
        .....I..
        .....I..`
     );
