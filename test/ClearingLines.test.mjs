0dbacd2798068a567b8d611e2aa5048ce971f85d
Roni Tuohino <rt@ronituohino.com>
2026-03-05 11:24:42 +0200

    Keep writing tests

--- test/ClearingLines.test.mjs
+++ test/ClearingLines.test.mjs
@@ -3,26 +3,30 @@ import { expect } from "chai";
 import { Board } from "../src/Board.mjs";
 
 const CUSTOM_I_BLOCK = [
-  `...
-   III
-   ...`,
-  `.I.
-   .I.
-   .I.`,
-  `...
-   III
-   ...`,
-  `.I.
-   .I.
-   .I.`,
+  `....
+   IIII
+   ....
+   ....`,
+  `.I..
+   .I..
+   .I..
+   .I..`,
+  `....
+   IIII
+   ....
+   ....`,
+  `.I..
+   .I..
+   .I..
+   .I..`,
 ];
 
-describe("When a single line (row) becomes full on the board", () => {
+describe("On the board, when line(s) become full", () => {
   let board;
   beforeEach(() => {
     board = new Board(6, 6);
   });
-  test("the line is removed", () => {
+  test("a single line is removed and blocks move down", () => {
     board.seed(
       `......
        ......
@@ -44,16 +48,70 @@ describe("When a single line (row) becomes full on the board", () => {
       `......
        ......
        ......
-       ......
+       ....I.
        T...II
        TTT.II`
     );
   });
-  test.skip("blocks above the line are moved down", () => {});
+
+  test("two lines are cleared and blocks move down", () => {
+    board.seed(
+      `......
+       ......
+       ......
+       T....I
+       IIII.I
+       TTTT.I`,
+      CUSTOM_I_BLOCK,
+      3,
+      0,
+      1
+    );
+
+    for (let i = 0; i < 10; i++) {
+      board.tick();
+    }
+
+    expect(board.toString()).to.equalShape(
+      `......
+       ......
+       ......
+       ......
+       ....I.
+       T...II`
+    );
+  });
+  test("three lines are cleared and blocks move down", () => {
+    board.seed(
+      `......
+       ...T..
+       .OTT..
+       TOOT.I
+       IIII.I
+       TTTT.I`,
+      CUSTOM_I_BLOCK,
+      3,
+      0,
+      1
+    );
+
+    for (let i = 0; i < 10; i++) {
+      console.log(board.toString());
+      board.tick();
+    }
+
+    expect(board.toString()).to.equalShape(
+      `......
+       ......
+       ......
+       ......
+       ...T..
+       .OTTI.`
+    );
+  });
+  test("four lines are cleared and blocks move down", () => {});
 });
 
-describe("When N lines become full", () => {
-  test.skip("N=2 lines are removed", () => {});
-  test.skip("N=3 lines are removed", () => {});
-  test.skip("N=4 lines are removed", () => {});
+describe("When a line doesn't become full", () => {
+  test.skip("do not remove any lines or move blocks", () => {});
 });
