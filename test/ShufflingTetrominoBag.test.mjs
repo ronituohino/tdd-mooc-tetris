d8259d008c37b2ed73ef5092ef796d20bab8a89f
Roni Tuohino <rt@ronituohino.com>
2026-03-10 06:46:37 +0200

    Make shuffle test pass

--- test/ShufflingTetrominoBag.test.mjs
+++ test/ShufflingTetrominoBag.test.mjs
@@ -3,20 +3,25 @@ import { expect } from "chai";
 import { Tetromino } from "../src/Tetromino.mjs";
 import { TetrominoBag } from "../src/TetrominoBag.mjs";
 
+const setOfTetrominos = [Tetromino.I_SHAPE, Tetromino.T_SHAPE, Tetromino.O_SHAPE];
+
 describe("A tetromino bag", () => {
   let bag;
-  const resetBag = () => {
-    bag = new TetrominoBag();
-    bag.add([Tetromino.I_SHAPE, Tetromino.I_SHAPE, Tetromino.T_SHAPE, Tetromino.T_SHAPE, Tetromino.O_SHAPE]);
-  };
   beforeEach(() => {
-    resetBag();
+    bag = new TetrominoBag();
   });
 
   test("can be initialized with n tetrominos", () => {
-    expect(bag.length).to.equal(5);
+    for (let n = 0; n < 100; n++) {
+      bag = new TetrominoBag();
+      for (let i = 0; i < n; i++) {
+        bag.add([setOfTetrominos[Math.floor(Math.random() * 3)]]);
+      }
+      expect(bag.length).to.equal(n);
+    }
   });
   test("can give out tetrominos in LIFO order", () => {
+    bag.add(setOfTetrominos);
     const tetromino = bag.get();
     expect(tetromino.toString()).to.equalShape(
       `....
@@ -26,21 +31,33 @@ describe("A tetromino bag", () => {
     );
   });
   test("loses tetrominos when it gives them out", () => {
+    bag.add(setOfTetrominos);
     bag.get();
-    expect(bag.length).to.equal(4);
+    expect(bag.length).to.equal(2);
   });
   test("can be shuffled", () => {
-    let foundRandom = false;
-    while (!foundRandom) {
+    const uniqueSets = new Set();
+
+    for (let n = 0; n < 1000; n++) {
+      bag = new TetrominoBag();
+      bag.add(setOfTetrominos);
       bag.shuffle();
-      const tetromino = bag.get();
-      expect(tetromino.toString()).to.not.equalShape(
-        `....
-       .OO.
-       .OO.
-       ....`
-      );
-      foundRandom = true;
+
+      let threeTetrominoesString = "";
+      for (let i = 0; i < 3; i++) {
+        threeTetrominoesString += bag.get().toString();
+      }
+      uniqueSets.add(threeTetrominoesString);
     }
+
+    expect(uniqueSets.size).to.equal(6);
+    /**
+     * ITO
+     * IOT
+     * TIO
+     * TOI
+     * OIT
+     * OTI
+     */
   });
 });
